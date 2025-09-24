e6d11933800042cdde34feaadeb7f481ef9ab4aa
2300030756-SUHITHA <162713778+2300030756-SUHITHA@users.noreply.github.com>
2025-09-25 00:56:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <ToastProvider>
       <AuthProvider>
-        <Router basename="/FMS">
+        <Router basename="/frontapp/">
           <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
             <Routes>
               <Route path="/" element={<Navigate to="/login" replace />} />
